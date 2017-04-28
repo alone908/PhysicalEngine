d2137718d3ee581ead2e05c61184d91076befd9a
Chung Jun <Chung@Chung-Jun-iMac.Home>
2017-04-28 14:06:10 +0800

    apply victor.js , finish bounce to center

--- js/ball.js
+++ js/ball.js
@@ -1,39 +1,56 @@
 $(document).ready(function(){
 
-
   var canvas = document.getElementById( 'canvas' );
   var ctx = canvas.getContext('2d');
   ctx.translate(canvas.width * 0.5, canvas.height * 0.5);
   ctx.transform(1, 0, 0, -1, 0, 0);
 
-  var ball = {cx:-250,cy:0,r:15,vector:{x:0,y:1}};
+  if(Math.random() < 0.5){var sign1=-1}else { var sign1=1 }
+  if(Math.random() < 0.5){var sign2=-1}else { var sign2=1 }
+
+  var initialX = sign1*Math.random();
+  var initialY = sign2*Math.sqrt( 1-initialX*initialX );
+  var initialVector = new Victor(initialX,initialY);
+
+  var ball = {cx:0,
+              cy:0,
+              r:15,
+              speed:1,
+              vector:initialVector};
 
   var lines = [
     {x1:-300,y1:150,x2:-150,y2:300},
     {x1:150,y1:300,x2:300,y2:150},
     {x1:150,y1:-300,x2:300,y2:-150},
     {x1:-300,y1:-150,x2:-150,y2:-300},
-
-    {x1:-290,y1:150,x2:-290,y2:-150},
-    {x1:-150,y1:290,x2:150,y2:290},
-    {x1:290,y1:150,x2:290,y2:-150},
-    {x1:-150,y1:-290,x2:150,y2:-290}
+    {x1:-300,y1:150,x2:-300,y2:-150},
+    {x1:-150,y1:300,x2:150,y2:300},
+    {x1:300,y1:150,x2:300,y2:-150},
+    {x1:-150,y1:-300,x2:150,y2:-300},
+    {x1:-250,y1:50,x2:-150,y2:200},
+    {x1:123,y1:-66,x2:-59,y2:73},
+    {x1:-13,y1:-266,x2:36,y2:73},
+    {x1:-214,y1:189,x2:-142,y2:-34},
+    {x1:84,y1:69,x2:142,y2:199},
+    {x1:84,y1:209,x2:142,y2:9},
   ]
 
   lines.forEach(function(line,i){
-    lines[i].length = lineLength(lines[i].x1,lines[i].y1,lines[i].x2,lines[i].y2);
-    lines[i].angle = lineAngleToAxis(lines[i].x1,lines[i].y1,lines[i].x2,lines[i].y2);
+    lines[i].vector = new Victor(lines[i].x2-lines[i].x1,lines[i].y2-lines[i].y1);
   });
-
-  console.log(lines);
 
   drawGameWorld();
   drawBall();
 
-  function moveBall(){
-    ball.cx += ball.vector.x;
-    ball.cy += ball.vector.y;
-  }
+  setInterval(function(){
+
+    clearCanvas();
+    moveBall();
+    collisionDetect(lines);
+    drawGameWorld();
+    drawBall();
+
+  },1)
 
   function drawGameWorld(){
     lines.forEach(function(line,i){
@@ -50,73 +67,39 @@ $(document).ready(function(){
     ctx.stroke();
   }
 
-  function clearCanvas(){
-    ctx.clearRect(-300, -300, 600, 600);
+  function moveBall(){
+    ball.cx += ball.vector.x*ball.speed;
+    ball.cy += ball.vector.y*ball.speed;
   }
 
   function collisionDetect(lines){
 
     lines.forEach(function(line,i){
 
-      var distoPoint1 = lineLength(ball.cx,ball.cy,line.x1,line.y1);
-      var distoPoint2 = lineLength(ball.cx,ball.cy,line.x2,line.y2);
+      var distoPoint1 = new Victor(line.x1-ball.cx,line.y1-ball.cy).length();
+      var distoPoint2 = new Victor(line.x2-ball.cx,line.y2-ball.cy).length();
 
-      if(line.length.toFixed(1) === (distoPoint1+distoPoint2).toFixed(1)){
-        console.log('collision');
+      if(line.vector.length().toFixed(1) === (distoPoint1+distoPoint2).toFixed(1)){
+        console.log('bouncing');
         bouncing(line);
         return false;
       }
-
     })
 
   }
 
   function bouncing(line){
 
-    var vectorLength = lineLength(0,0,ball.vector.x,ball.vector.y);
-    var vectorAngle = lineAngleToAxis(0,0,ball.vector.x,ball.vector.y);
+    var enterAngle = ball.vector.horizontalAngleDeg()-line.vector.horizontalAngleDeg();
 
-
-    var newX = -ball.vector.x;
-    var newY = -ball.vector.y;
-
-    //
-    // var enterAngle = vectorAngle-line.angle;
-    // var leaveAngle = enterAngle;
-    //
-    // console.log('vector angle:'+vectorAngle);
-    // console.log('enter angle:'+enterAngle);
-    //
-    // var newX = (ball.vector.x*Math.cos(2*leaveAngle*Math.PI/180)-ball.vector.y*Math.sin(2*leaveAngle*Math.PI/180));
-    // var newY = (ball.vector.x*Math.sin(2*leaveAngle*Math.PI/180)+ball.vector.y*Math.cos(2*leaveAngle*Math.PI/180));
-
-    var reflectVector = {x:newX,y:newY}
-    console.log(reflectVector);
-    ball.vector = reflectVector;
+    console.log('enter vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
+    ball.vector = ball.vector.rotateDeg(-2*enterAngle);
+    console.log('bounce vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
 
   }
 
-
-  function lineLength(x1,y1,x2,y2){
-    return Math.hypot( (x2-x1), (y2-y1) );
+  function clearCanvas(){
+    ctx.clearRect(-300, -300, 600, 600);
   }
-
-  function lineAngleToAxis(x1,y1,x2,y2){
-    var dy = y2 - y1;
-    var dx = x2 - x1;
-    var theta = Math.atan2(dy, dx); // range (-PI, PI]
-    theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-    return theta;
-  }
-
-  setInterval(function(){
-
-    clearCanvas();
-    moveBall();
-    collisionDetect(lines);
-    drawGameWorld();
-    drawBall();
-
-  },1)
 
 })
