0b60066351fff99dac6831c9b295955aec79f682
Chung Jun <Chung@Chung-Jun-iMac.Home>
2017-05-05 22:27:00 +0800

    update code

--- js/ball.js
+++ js/ball.js
@@ -36,21 +36,31 @@ $(document).ready(function(){
   ]
 
   lines.forEach(function(line,i){
-    lines[i].vector = new Victor(lines[i].x2-lines[i].x1,lines[i].y2-lines[i].y1);
+    lines[i].vector = new Victor(line.x2-line.x1,line.y2-line.y1);
+    lines[i].slope = ( (line.y2-line.y1)/(line.x2-line.x1) ).toFixed(1);
+    // if( Math.abs(lines[i].slope) === Infinity ){
+    //
+    // }
+    lines[i].constant = ( line.y1-( (line.y2-line.y1)/(line.x2-line.x1) )*line.x1 ).toFixed(1);
+    // if( Math.abs(lines[i].constant) === Infinity ){
+    //   lines[i].constant = line.x1;
+    // }
   });
-
+  console.log(lines);
   drawGameWorld();
   drawBall();
 
   setInterval(function(){
 
     clearCanvas();
-    moveBall();
+    for(var i=1;i<=ball.speed;i++){
+      moveBall();
+    }
     collisionDetect(lines);
     drawGameWorld();
     drawBall();
 
-  },1)
+  },10)
 
   function drawGameWorld(){
     lines.forEach(function(line,i){
@@ -70,8 +80,8 @@ $(document).ready(function(){
   }
 
   function moveBall(){
-    ball.cx += ball.vector.x*ball.speed;
-    ball.cy += ball.vector.y*ball.speed;
+    ball.cx += ball.vector.x;
+    ball.cy += ball.vector.y;
   }
 
   function collisionDetect(lines){
@@ -81,22 +91,137 @@ $(document).ready(function(){
       var distoPoint1 = new Victor(line.x1-ball.cx,line.y1-ball.cy).length();
       var distoPoint2 = new Victor(line.x2-ball.cx,line.y2-ball.cy).length();
 
+      var parallelLine_constant = ball.cy-line.slope*ball.cx;
+
+      // if( Math.abs(parallelLine_constant) === Infinity ){
+      //   parallelLine_constant = ball.cx;
+      //   // console.log('here');
+      // }
+
+      var ballCenterInsideLine = true;
+
+      if( line.x1 > line.x2 ){
+          if(ball.cx > line.x1) ballCenterInsideLine = false;
+          if(ball.cx < line.x2) ballCenterInsideLine = false;
+      }else if (line.x2 > line.x1) {
+        if(ball.cx > line.x2) ballCenterInsideLine = false;
+        if(ball.cx < line.x1) ballCenterInsideLine = false;
+      }else if (line.x1 == line.x2) {
+
+      }
+
+      if( line.y1 > line.y2 ){
+          if(ball.cy > line.y1) ballCenterInsideLine = false;
+          if(ball.cy < line.y2) ballCenterInsideLine = false;
+      }else if (line.y2 > line.y1) {
+        if(ball.cy > line.y2) ballCenterInsideLine = false;
+        if(ball.cy < line.y1) ballCenterInsideLine = false;
+      }
+
+      if( Math.abs(line.slope) === Infinity ){
+
+        var distanceFromCenterToLine = Math.abs(ball.cx-line.x1);
+
+      }else {
+
+        var distanceFromCenterToLine = Math.abs(line.constant-parallelLine_constant)/Math.sqrt(1+line.slope*line.slope);
+      }
+
+
+      // console.log(ballCenterInsideLine);
+      // console.log(distanceFromCenterToLine);
+
+      if(ballCenterInsideLine){
+        // console.log(distanceFromCenterToLine);
+      }
+
+      if(ballCenterInsideLine && distanceFromCenterToLine <= ball.r){
+
+
+          console.log('bouncing on radius');
+
+          console.log( (distanceFromCenterToLine - ball.r).toFixed(1) );
+
+          if( distanceFromCenterToLine - ball.r === 0){
+
+            bouncing(line,false);
+
+          }else {
+
+            bouncing(line,true);
+          }
+
+
+          return false;
+
+
+      }
+
+
+
       if(line.vector.length().toFixed(1) === (distoPoint1+distoPoint2).toFixed(1)){
-        console.log('bouncing');
-        bouncing(line);
+
+
+
+        // console.log(parallelLine_constant);
+        // console.log( Math.abs(line.constant-parallelLine_constant) )
+
+        // console.log('bouncing');
+        // bouncing(line);
         return false;
+
       }
     })
 
   }
 
-  function bouncing(line){
+  function predictIntersect(lines){
+    lines.forEach(function(line,i){
+
+      var slope2 = (line.y2-line.y1)/(line.x2-line.x1);
+      var constant2 = line.y1-slope2*line.x1;
+
+      L2: y=slope
+
+    })
+  }
+
+  function bouncing(line,crossLine){
 
     var enterAngle = ball.vector.horizontalAngleDeg()-line.vector.horizontalAngleDeg();
+    if(Math.abs(enterAngle) < 5 && Math.abs(enterAngle) >= 0) enterAngle = 5;
+    if(Math.abs(enterAngle) > -5 && Math.abs(enterAngle) < 0) enterAngle = -5;
 
-    console.log('enter vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
+    // console.log('enter vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
     ball.vector = ball.vector.rotateDeg(-2*enterAngle);
-    console.log('bounce vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
+    // console.log('bounce vector:(x:'+ball.vector.x+',y:'+ball.vector.y+')');
+    console.log(crossLine);
+    if(crossLine){
+      console.log('cross line');
+      var keepMoving = true;
+      while (keepMoving) {
+
+        moveBall();
+
+        var parallelLine_constant = ball.cy-line.slope*ball.cx;
+      if( Math.abs(line.slope) === Infinity ){
+
+        var distanceFromCenterToLine = Math.abs(ball.cx-line.x1);
+
+      }else {
+
+        var distanceFromCenterToLine = Math.abs(line.constant-parallelLine_constant)/Math.sqrt(1+line.slope*line.slope);
+      }
+
+
+
+        if(distanceFromCenterToLine > ball.r){
+          console.log(distanceFromCenterToLine);
+          keepMoving = false
+        }
+
+      }
+    }
 
   }
 
